776a239eb6426fdf8270d6e38b2b8f54cd2eb851
nikhat25 <nikhatpravin0866@gmail.com>
2026-09-09 21:07:06 +0530

    Reverse String

---
+++ Second.java
@@ -0,0 +1,18 @@
+
+public class Second {
+    public static void main(String args[]){
+        String s="Nikhat";
+        char ch[]=s.toCharArray();
+        int i=0;
+        int j=ch.length-1;
+        while(i<j){
+            char temp=ch[i];
+            ch[i]=ch[j];
+            ch[j]=temp;
+            i++;
+            j--;
+        }
+        
+        System.out.print(ch);
+    }
+}
